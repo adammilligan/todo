5e767d867288eccf043cc13af362d9279ba41c3f
adammilligan <millgan83949@gmail.com>
2024-11-12 14:50:22 +0300

    add styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,14 @@ import {persistor, setupStore} from './redux/store.ts';
 import {Provider} from "react-redux";
 import {PersistGate} from "redux-persist/integration/react";
 import AppRouter from "./components/main/AppRouter.tsx";
-import {ThemeProvider} from "@mui/material";
-import {mainTheme} from "./config/themes";
 
 function App() {
     return (
-        <ThemeProvider theme={mainTheme}>
-            <Provider store={setupStore}>
-                <PersistGate persistor={persistor}>
-                    <AppRouter/>
-                </PersistGate>
-            </Provider>
-        </ThemeProvider>
+        <Provider store={setupStore}>
+            <PersistGate persistor={persistor}>
+                <AppRouter/>
+            </PersistGate>
+        </Provider>
     )
 }
 
--- src/views/toggle/index.tsx
+++ src/views/toggle/index.tsx
@@ -1,7 +1,16 @@
-import { Accordion, AccordionDetails, AccordionSummary, Box, Button, TextField, Typography, Checkbox } from "@mui/material"; // Import Checkbox from @mui/material
-import { useAppDispatch, useAppSelector } from "../../redux/hooks.ts";
-import { add, toggleDone } from "../../redux/slices/event.ts";
-import { useState } from "react";
+import {
+    Accordion,
+    AccordionDetails,
+    AccordionSummary,
+    Box,
+    Button,
+    TextField,
+    Typography,
+    Checkbox,
+} from "@mui/material";
+import {useAppDispatch, useAppSelector} from "../../redux/hooks.ts";
+import {add, toggleDone} from "../../redux/slices/event.ts";
+import {useState} from "react";
 
 const ViewsToggle = () => {
     const state = useAppSelector(state => state.events);
@@ -10,7 +19,7 @@ const ViewsToggle = () => {
 
     const addEventHandler = () => {
         if (inputValue.trim()) {
-            dispatch(add(inputValue)); // Ensure the task has an isDone property
+            dispatch(add(inputValue));
             setInputValue('');
         }
     }
@@ -20,9 +29,9 @@ const ViewsToggle = () => {
     }
 
     return (
-        <Box display="flex" flexDirection="column" sx={{ height: '100vh', justifyContent: 'flex-start' }}>
+        <Box display="flex" flexDirection="column" sx={{height: '100vh', justifyContent: 'flex-start'}}>
             <Typography variant='h2' color='primary'>Список задач</Typography>
-            <Box display="flex" alignItems="center" sx={{ alignItems: "start", m: 2 }}>
+            <Box display="flex" alignItems="center" sx={{alignItems: "start", m: 2}}>
                 <TextField
                     variant="outlined"
                     value={inputValue}
@@ -30,8 +39,9 @@ const ViewsToggle = () => {
                     sx={{
                         width: '800px',
                         height: '56px',
-                        '& .MuiInputBase-root': { borderRadius: '8px 0 0 8px' },
-                        '& .MuiOutlinedInput-notchedOutline': { borderRadius: '8px 0 0 8px' }
+                        color: '#000',
+                        '& .MuiInputBase-root': {borderRadius: '8px 0 0 8px'},
+                        '& .MuiOutlinedInput-notchedOutline': {borderRadius: '8px 0 0 8px'}
                     }}
                 />
                 <Button
@@ -39,26 +49,29 @@ const ViewsToggle = () => {
                     onClick={addEventHandler}
                     sx={{
                         height: '56px',
-                        '&:last-child': { borderRadius: '0 8px 8px 0' }
+                        '&:last-child': {borderRadius: '0 8px 8px 0'}
                     }}
                 >
                     Добавить
                 </Button>
             </Box>
-            <Accordion sx={{ marginBottom: '20px', borderRadius: '8px' }}>
+            <Accordion sx={{marginBottom: '20px', borderRadius: '8px'}}>
                 <AccordionSummary>
-                    <Typography variant='subtitle1'>Список задач</Typography>
+                    <Typography variant='subtitle1'>Все задачи</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                     <Box display="flex" flexDirection="column">
-                        {state.data.map((task) => (
-                            <Box key={task.id} display="flex" alignItems="center">
+                        {state.data.sort().map((task) => (
+                            <Box sx={{ cursor: 'pointer' }}
+                                 key={task.id}
+                                 display="flex"
+                                 alignItems="center"
+                                 onClick={() => toggleTaskCompletion(task.id)}>
                                 <Checkbox
                                     checked={task.isDone}
-                                    onChange={() => toggleTaskCompletion(task.id)}
                                 />
                                 <Typography
-                                    sx={{ textDecoration: task.isDone ? 'line-through' : 'none' }}
+                                    sx={{textDecoration: task.isDone ? 'line-through' : 'none'}}
                                 >
                                     {task.description}
                                 </Typography>
@@ -67,23 +80,41 @@ const ViewsToggle = () => {
                     </Box>
                 </AccordionDetails>
             </Accordion>
-            <Accordion sx={{ marginBottom: '20px', borderRadius: '8px' }}>
+            <Accordion sx={{marginBottom: '20px', borderRadius: '8px'}}>
                 <AccordionSummary>
                     <Typography variant='subtitle1'>Невыполненные задачи</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                     {state.data.filter(task => !task.isDone).map((task) => (
-                        <Typography key={task.id}>{task.description}</Typography>
+                        <Box key={task.id} display="flex" alignItems="center">
+                            <Checkbox
+                                checked={task.isDone}
+                            />
+                            <Typography
+                                sx={{textDecoration: task.isDone ? 'line-through' : 'none'}}
+                            >
+                                {task.description}
+                            </Typography>
+                        </Box>
                     ))}
                 </AccordionDetails>
             </Accordion>
-            <Accordion sx={{ marginBottom: '20px', borderRadius: '8px' }}>
+            <Accordion sx={{marginBottom: '20px', borderRadius: '8px'}}>
                 <AccordionSummary>
                     <Typography variant='subtitle1'>Выполненные задачи</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                     {state.data.filter(task => task.isDone).map((task) => (
-                        <Typography key={task.id}>{task.description}</Typography>
+                        <Box key={task.id} display="flex" alignItems="center">
+                            <Checkbox
+                                checked={task.isDone}
+                            />
+                            <Typography
+                                sx={{textDecoration: task.isDone ? 'line-through' : 'none'}}
+                            >
+                                {task.description}
+                            </Typography>
+                        </Box>
                     ))}
                 </AccordionDetails>
             </Accordion>
